cc80dd9bf792f268057f35eb7ab139307634ff0c
xixky <1487211167@qq.com>
2026-07-04 23:36:11 +0800

    Update diy-part1.sh

--- config/immortalwrt_master/diy-part1.sh
+++ config/immortalwrt_master/diy-part1.sh
@@ -8,9 +8,11 @@
 
 # Add a custom feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
 git clone --depth=1 https://github.com/EasyTier/luci-app-easytier.git package/luci-app-easytier
 
+# 克隆 luci-app-mosdns v5 及其依赖
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # Remove unnecessary packages
 # rm -rf package/emortal/{autosamba,ipv6-helper}
